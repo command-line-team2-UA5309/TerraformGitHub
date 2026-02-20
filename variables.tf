938b860e5e5b61c59db89f4d43edccc6a2868a6c
Kateryna Hrinchak <92380648+KatyaHrinchak@users.noreply.github.com>
2026-02-20 19:28:08 +0200

    feat:CLT-58 change django backend repo name

--- variables.tf
+++ variables.tf
@@ -7,5 +7,5 @@ variable "github_token" {
 variable "repository_names" {
   description = "List of names for GitHub repositories"
   type        = set(string)
-  default     = ["TerraformAWS", "AnsiblePlaybooks", "Django_Backend"]
+  default     = ["TerraformAWS", "AnsiblePlaybooks", "DjangoBackend"]
 }
