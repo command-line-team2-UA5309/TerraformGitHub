b052756f977e4fa75770c802e1a562480c4a750b
d-sarapaniuk <d.sarapaniuk@gmail.com>
2026-02-27 17:33:04 +0200

    chore:CLT-71 imported the newly created InfrastructureSecret repo, added a new repo to create and updated README.md

--- variables.tf
+++ variables.tf
@@ -8,10 +8,12 @@ variable "repositories" {
   description = "GitHub repository names and corresponding required checks(linters) for each repository main branch"
   type        = map(set(string))
   default = {
-    VueFrontend       = ["Markdown lint:15368"]
-    DjangoBackend     = ["Markdown lint:15368"]
-    MapService        = ["Markdown lint:15368"]
-    EmailMicroservice = ["Markdown lint:15368"]
-    DayPassService    = ["Markdown lint:15368", "Run golangci-lint:15368"]
+    VueFrontend          = ["Markdown lint:15368"]
+    DjangoBackend        = ["Markdown lint:15368"]
+    MapService           = ["Markdown lint:15368"]
+    EmailMicroservice    = ["Markdown lint:15368"]
+    DayPassService       = ["Markdown lint:15368", "Run golangci-lint:15368"]
+    InfrastructureSecret = ["Markdown lint:15368"]
+    IAC_AWS              = ["Markdown lint:15368"]
   }
 }
